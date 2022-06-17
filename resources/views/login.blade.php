546ade1ae349076696684cc5fcaf28fa0b8bc64d
fasnasalih <fasnapp26@gmail.com>
2022-06-17 20:56:35 +0530

    comt

---
+++ resources/views/login.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <h1></h1> 
+   <table>
+       <tr>
+       <th colspan="2" > LOGIN OR SIGN UP </th>
+       </tr>
+      <tr>
+          <td>Name</td>
+          <td></td>
+      </tr>
+      <tr>
+          <td>Password</td>
+          <td></td>
+      </tr>
+   </table>
+</body>
+</html>
